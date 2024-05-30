1413f04139fa7ce8f856034550ef8d88e049ed75
Pabatso-Raphoto <ST10452650@imconnect.edu.za>
2024-05-30 10:00:32 +0200

    Update TaskClass.java

--- src/poe_part2/TaskClass.java
+++ src/poe_part2/TaskClass.java
@@ -10,19 +10,59 @@ package poe_part2;
  */
 public class TaskClass {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-     //Method to add new Task    
-  
     
+     private String TaskName; 
+     private int taskNumber; 
+     private String taskDescription;
+     private String developerFirstName;
+     private String developerLasttName;
+     private int taskDuration;
+     private String taskID;
+     private String taskStatus;
+     
+     public Task(String TaskName, int taskNumber,String taskDescription, String developerFirstName, String developerLastName, int taskDuration, 
+              String taskStatus){
+     this.TaskName = TaskName;
+     this.taskNumber = taskNumber;
+     this.taskDescription = taskDescription;
+     this.developerFirstName = developerFirstName;
+     this.developerLasttName = developerLastName;
+     this.taskDuration = taskDuration;
+     this.taskID = taskID;
+     this.taskStatus = taskStatus;     
+     }
     
-    
-    
-    
-    
-    }
+     public String createTaskDescription(){
+         return taskDescription()<=50;
+                 }
+         
+     public String createTaskID(){
+         
+         //Get first 2 letters of task name and change to uppercase 
+         String TaskName1 = TaskName.substring(0,2).toUpperCase();
+         
+         //Get last 3 letters of developer's last name and put in uppsercase
+         String developerLastName1 = developerLastName.substring(developerLastName.length()-3) ;
+         
+         //Combination of the TaskName1 and developLastname1 with colons between them
+         String taskID = TaskName1 + ":" + taskNumber + ":" + developerLastName1;
+         
+         return taskID;
+     }
+     
+     public String printTaskDetails(){
+         return "Task Status is: " + taskStatus + "\n" + 
+                 "Developer Details: " + developerFirstName + " " + developerLastName + "\n" + 
+                 "Task Number: " + taskNumber + "\n" + 
+                 "Task Name: " + TaskName + "\n"+ 
+                 "Task Description: " + taskDescription + "\n" +
+                 "Task ID: " + taskID + "\n" +
+                 "Task Duration: " + taskDuration + "Hours";
+     }
+     
+     public int returnTotalHours(){
+         return TaskDuration;
+     }
+}
     
 }
